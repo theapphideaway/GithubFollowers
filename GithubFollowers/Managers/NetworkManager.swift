d84f27bc14b21a66678100e8a2f47d7b5677772e
Ian Schoenrock <ischoenrock@lumina247.net>
2020-01-07 12:00:00 -0500

    Finished the network call and error handling

--- GithubFollowers/Managers/NetworkManager.swift
+++ GithubFollowers/Managers/NetworkManager.swift
@@ -13,7 +13,41 @@ class NetworkManager {
     let baseURL = "https://api.github.com/users/"
     private init(){}
     
-    func getFollowers(for username: String, page: String, completed: @escaping ([Follower]?, String?) -> Void){
-        let endpoint = baseURL + ""
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void){
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else{
+            completed(nil, "This username created an invalide request, Please try again.")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            if let _ = error{
+                completed(nil, "Unable to complete your request. Pease check your internet connection.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+                completed(nil, "Invalid response from the server. Please try again.")
+                return
+            }
+            
+            guard let data = data else{
+                completed(nil, "The data recieved from the server was invalid. Please try again.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            }catch{
+                completed(nil, "The data recieved from the server was invalid. Please try again.")
+                return 
+            }
+        }
+        
+        task.resume()
     }
 }
